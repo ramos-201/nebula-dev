2d172b4dae416dac09c253186f7a65029e592710
Ramos <andfera201@gmail.com>
2025-04-03 19:47:40 -0500

    feat: Added functionality to retrieve the  path in Linux for command history capture.

--- src/app.rs
+++ src/app.rs
@@ -1,10 +1,15 @@
 use std::env;
+use std::path::PathBuf;
+
+const _COMMAND_HISTORY_PATH: &str = ".nebula_history";
 
 pub fn run() {
     let args: Vec<String> = env::args().collect();
 
     if let Some(command) = _get_command(&args) {
-        print!("{}", command)
+        let command_history_path = _get_command_history_linux();
+        print!("{}", command);
+        println!("{:?}", command_history_path);
     }
 }
 
@@ -14,6 +19,14 @@ fn _get_command(args: &[String]) -> Option<String> {
     } else {
         None
     }
+}
+
+fn _get_command_history_linux() -> PathBuf {
+
+    let home_dir = env::var("HOME").expect("HOME environment variable not set");
+    let mut path = PathBuf::from(home_dir);
+    path.push(_COMMAND_HISTORY_PATH);
+    path
 }
 
 #[cfg(test)]
@@ -48,5 +61,12 @@ mod tests {
         let args = vec!["program".to_string()];
 
         assert_eq!(_get_command(&args), None);
+    }
+
+    #[test]
+    fn test_get_command_history_path_it_should_return_correct_path() {
+        let home_dir = env::var("HOME").unwrap_or_default();
+        let expected_path = format!("{}/{}", home_dir, _COMMAND_HISTORY_PATH);
+        assert_eq!(_get_command_history_linux().to_string_lossy(), expected_path);
     }
 }
